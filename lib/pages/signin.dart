9ebf56ff94525bcb537267b3e6107c596492340b
PRASHANTH <prashanthreddy9948379663@gmail.com>
2020-07-10 22:37:36 +0530

    added form validation for signing pages

--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -5,26 +5,34 @@ import 'package:kirana/widgets/OkButton.dart';
 import 'package:kirana/widgets/TextFieldWidget.dart';
 import 'package:kirana/pages/signup.dart';
 
-class SignInPage extends StatelessWidget {
-  final name = 'signin';
+class SignInPage extends StatefulWidget {
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
 
+class _SignInPageState extends State<SignInPage> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Form(
+      key: _formKey,
+      child: Scaffold(
         body: Container(
       child: ListView(
         children: <Widget>[
           Column(
             children: <Widget>[
               DescriptionText('Sign In'),
-              TextFieldWidget('Email'),
-              TextFieldWidget('Password'),
+              TextFieldWidgetWithValidation('Email'),
+              TextFieldWidgetWithValidation('Password'),
               OkButton('Ok'),
               RedirectPage(SignUpPage(), 'Sign Up', 'give it a shot')
             ],
           )
         ],
       ),
-    ));
+    )
+    )
+    );
   }
 }
