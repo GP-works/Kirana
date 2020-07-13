cdb3be856d855030ed3a5aca494c9c680e0e584f
prudhvi-hack <gadupudiprudhvi@gmail.com>
2020-07-13 23:14:29 +0530

    added a form to add menu item

--- lib/pages/edititems.dart
+++ lib/pages/edititems.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:kirana/widgets/ImagePicker.dart';
+import 'package:kirana/widgets/TextFieldWidget.dart';
 
 class EditItemsPage extends StatelessWidget {
   final name = 'edititems';
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(child: Text("EditItems page")),
-      appBar: AppBar(title: Text("EditItems")),
-    );
+        appBar: AppBar(title: Text("EditItems")),
+        body: Form(
+          key: _formKey,
+          child: ListView(children: [
+            TextFieldWidgetWithValidation('Name'),
+            NumberFieldWidgetWithValidation('price'),
+            NumberFieldWidgetWithValidation('original price'),
+            MultilineTextWidgetWithValidation('description'),
+            ItemImagePicker(),
+          ]),
+        ));
   }
 }
