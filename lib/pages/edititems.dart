4700590ffe29e11e5faf153c23e9e8e06a080fb4
prudhvi-hack <gadupudiprudhvi@gmail.com>
2020-07-15 09:51:20 +0530

    made button more responsive

--- lib/pages/edititems.dart
+++ lib/pages/edititems.dart
@@ -5,6 +5,8 @@ import 'package:kirana/widgets/ImagePicker.dart';
 import 'package:kirana/widgets/TextFieldWidget.dart';
 import 'package:kirana/models/Item.dart';
 import 'package:provider/provider.dart';
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class EditItemsPageForm extends StatefulWidget {
   @override
@@ -19,6 +21,7 @@ class _EditItemsPageFormState extends State<EditItemsPageForm> {
   double price;
   double originalPrice;
   String imageurl = '';
+  File itemimage;
   int id;
   final nameController = TextEditingController();
   final priceController = TextEditingController();
@@ -34,6 +37,40 @@ class _EditItemsPageFormState extends State<EditItemsPageForm> {
     descriptionController.dispose();
     super.dispose();
   }
+  FirebaseStorage _storage = FirebaseStorage.instance;
+  Future<String> uploadImage() async {
+    StorageReference reference = _storage.ref().child('images/');
+    StorageUploadTask uploadTask = reference.child("${nameController.text}${DateTime.now().millisecondsSinceEpoch}").putFile(itemimage);
+    if (uploadTask.isSuccessful || uploadTask.isComplete) {
+      final String url = await reference.getDownloadURL();
+      print("The download URL is " + url);
+      setState(() {
+        imageurl=url;
+      });
+    } else if (uploadTask.isInProgress) {
+      uploadTask.events.listen((event) {
+        double percentage = 100 *
+            (event.snapshot.bytesTransferred.toDouble() /
+                event.snapshot.totalByteCount.toDouble());
+        print("THe percentage " + percentage.toString());
+      });
+
+      StorageTaskSnapshot storageTaskSnapshot = await uploadTask.onComplete;
+      imageurl = await storageTaskSnapshot.ref.getDownloadURL();
+
+
+      //Here you can get the download URL when the task has been completed.
+      print("Download URL " + imageurl.toString());
+    } else {
+      Scaffold.of(context).showSnackBar(new SnackBar(
+        content: new Text("Error uploading pic"),
+        backgroundColor: Colors.red,
+      ));
+      return imageurl;
+    }
+
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -51,44 +88,47 @@ class _EditItemsPageFormState extends State<EditItemsPageForm> {
               padding: EdgeInsets.fromLTRB(0, 10, 20, 0),
               child: RaisedButton(
                   child: Text(
-                    "Next".toUpperCase(),
+                    "Submit".toUpperCase(),
                     style: TextStyle(color: Colors.white, letterSpacing: 1),
                   ),
                   color: Colors.green[700],
-                  onPressed: () {
-                      if (_formKey.currentState.validate()) {
-                        itemname = nameController.text;
-                        price = double.parse(priceController.text);
-                        originalPrice = double.parse(mrpController.text);
-                        description = descriptionController.text;
-                        id = new DateTime.now().millisecondsSinceEpoch;
-                        if (imageurl == "") {
-                          Scaffold.of(context).showSnackBar(
-                              SnackBar(content: Text("select an image")));}
-                        else {
-                          _additem_to_container();
-
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => ItemsPage()));
-                        }
+                  onPressed: () async{
+                    await uploadImage();
+                    if (_formKey.currentState.validate()) {
+                      itemname = nameController.text;
+                      price = double.parse(priceController.text);
+                      originalPrice = double.parse(mrpController.text);
+                      description = descriptionController.text;
+                      id = new DateTime.now().millisecondsSinceEpoch;
+                      if (imageurl == "") {
+                        Scaffold.of(context).showSnackBar(
+                            SnackBar(content: Text("select an image")));
+                      } else {
+                        _additem_to_container();
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ItemsPage()));
                       }
-
+                    }
                   })),
         )
       ]),
     );
   }
 
-  void setUrl(String url) {
+  void setUrl(File image) {
     setState(() {
-      imageurl = url;
+      itemimage = image;
+      print("image setted");
+      if(itemimage==null){
+        print("done");
+      }
     });
   }
 
   void _additem_to_container() {
-    var catalog = Provider.of<ItemsModel>(context,listen: false);
+    var catalog = Provider.of<ItemsModel>(context, listen: false);
     catalog
         .add(Item(itemname, price, description, originalPrice, imageurl, id));
   }
